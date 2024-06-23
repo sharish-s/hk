d97aaca81dfc23365e56eea79c312a0da24b70b1
sharish-s <sharish.s@ya.ru>
2024-06-23 12:35:58 +0300

    upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+from random import choice
+from hamster_client import BOOST_ENERGY, HamsterClient, sleep, logging
+from config import ACCOUNTS, FEATURES
+from strings import DELIMITER
+
+clients = [HamsterClient(**options) for options in ACCOUNTS]
+
+def main():
+    while True:
+        for client in clients:
+            print(DELIMITER)
+            client.sync()
+            client.claim_daily_cipher()
+            client.tap()
+            client.buy_upgrades()
+            client.check_task()
+            client.claim_combo_reward()
+            if client.is_taps_boost_available:
+                client.boost(BOOST_ENERGY)
+            logging.info(client.log_prefix + " ".join(f"{k}: {v} |" for k, v in client.stats.items()))
+            print(DELIMITER)
+            sleep(choice(range(1, 10)))
+        delay_between_attempts = FEATURES.get('delay_between_attempts', 60 * 10)
+        sleep(choice(range(delay_between_attempts, delay_between_attempts + 60)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ config.py
@@ -0,0 +1,46 @@
+"""
+    FEATURES:
+        1. buy_upgrades -> управление покупкой карточек. 
+            True -> Включено, 
+            False -> Выключено
+        2. buy_decision_method -> метод покупки карточек (
+            - price -> покупать самую дешевую
+            - payback -> покупать ту, что быстрей всего окупится
+            - profit -> покупать самую прибыльну
+            - profitness -> покупать самую профитную (сколько добыча на каждый потраченный хома-рубль)
+            )
+        3. delay_between_attempts -> Задержка между заходами в секундах
+        4. num_purchases_per_cycle -> Кол-во покупок карточек за цикл
+        5. min_cash_value_in_balance -> Если баланс меньше этого карточки не покупать
+
+    ACCOUNTS:
+        name -> Название аккаунта. Так он будет виден в логе
+        token -> токен аккаунта
+        proxies -> настройки прокси, "кто знает - тот поймет". Если не нужен прокси, лучше убрать
+        buy_upgrades -> Описано в FEATURES, можно указать для каждого аккаунта отдельно
+        buy_decision_method -> Описано в FEATURES, можно указать для каждого аккаунта отдельно
+"""
+
+try:
+    from config_local import FEATURES, ACCOUNTS
+except:
+
+    FEATURES = {
+        "buy_upgrades": True,
+        "buy_decision_method": "payback",
+        "delay_between_attempts": 60 * 10,     # эта задержка будет применятся c доп. рандомом ( +/- 60сек )
+        "num_purchases_per_cycle": 5,
+        "min_cash_value_in_balance": 5_000_000,
+    }
+
+    ACCOUNTS = [
+        {"name": "account1", "token": "1234567890MySeCReTt0KeN1", "proxies": {"http": "http://user:pass@10.10.10.0:8080", "https": "https://user:pass@10.10.10.0:9090"}},
+        {"name": "account2", "token": "1234567890MySeCReTt0KeN2", "proxies": {"http": "http://user:pass@10.10.11.0:8080", "https": "https://user:pass@10.10.11.0:9090"}},
+        {"name": "account3", "token": "1234567890MySeCReTt0KeN3", "buy_upgrades": True, "buy_decision_method": "payback",},
+    ]
+
+for account in ACCOUNTS:
+    account['buy_upgrades'] = account.get('buy_upgrades', FEATURES.get('buy_upgrades', True))
+    account['buy_decision_method'] = account.get('buy_decision_method', FEATURES.get('buy_decision_method', 'payback'))
+    account['num_purchases_per_cycle'] = account.get('num_purchases_per_cycle', FEATURES.get('num_purchases_per_cycle'))
+    account['min_cash_value_in_balance'] = account.get('min_cash_value_in_balance', FEATURES.get('min_cash_value_in_balance', 0))
